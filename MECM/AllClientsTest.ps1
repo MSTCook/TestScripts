dadc3c23ab76f94dd2e77d3abe54484032c1cc55
Thomas Cook <thomascook@microsoft.com>
2025-02-27 12:05:03 -0500

    Initial Script

---
+++ MECM/AllClientsTest.ps1
@@ -0,0 +1,45 @@
+#requires -version 2
+<#
+.SYNOPSIS
+  Test script for MECM push
+
+.DESCRIPTION
+  This script writes to a file on the client computer and is intended to be pushed through MECM to
+  test funcitonality.
+
+.PARAMETER <Parameter_Name>
+  None
+
+.INPUTS
+  None
+
+.OUTPUTS
+  AllClientsTest.txt
+
+.NOTES
+  Version:        1.1
+  Author:         Thomas Cook
+  Creation Date:  2025.02.27
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  _AllClientsTest.ps1
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+$date = get-date
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+$RootPath = "$ENV:WinDir"
+$LogFolder = "CCM\Logs"
+$OutputFileName = "_AllClientsTest.log"
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+$FullPath = "$RootPath\$LogFolder"
+if(-not (Test-Path $FullPath)){
+  New-Item -Path $RootPath -Name $LogFolder -ItemType "directory"
+}
+
+Set-Content -Path "$FullPath\$OutputFileName" -Value $Date
